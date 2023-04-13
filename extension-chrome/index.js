416c62eefb19fc435da59def828f53199aecc8e4
vbrajon <vbrajon@gmail.com>
2023-04-13 12:43:01 +0200

    feat: add search across synced devices tabs

--- extension-chrome/index.js
+++ extension-chrome/index.js
@@ -36,14 +36,16 @@ window.idb = {
     return this.transaction("readwrite", (store) => store.clear())
   },
 }
-const app = createApp({
+window.app = createApp({
   data() {
     return {
       now: new Date().format("hh:mm"),
       search: "",
       history: Array(10).fill({}),
+      tabs: Array(10).fill({}),
       bookmarks: Array(10).fill({}),
       hoff: 0,
+      toff: 0,
       boff: 0,
     }
   },
@@ -52,6 +54,11 @@ const app = createApp({
       if (!this.search) return this.history
       const r = RegExp(this.search, "i")
       return this.history.filter((v) => ["title", "url"].some((k) => r.test(v[k])))
+    },
+    t() {
+      if (!this.search) return this.tabs
+      const r = RegExp(this.search, "i")
+      return this.tabs.filter((v) => ["title", "url"].some((k) => r.test(v[k])))
     },
     b() {
       if (!this.search) return this.bookmarks
@@ -63,25 +70,23 @@ const app = createApp({
     const flat = (v) => {
       if (v.length) return v.map(flat).flat()
       if (v.children) return flat(v.children)
-      return v
+      return { ...v, year: new Date(v.dateAdded).format("YYYY") }
     }
     const bookmarks = flat(await chrome.bookmarks.getTree.promisify()).sort("-dateAdded")
-    this.bookmarks = Object.freeze(
-      bookmarks.map((v) => {
-        v.year = new Date(v.dateAdded).format("YYYY")
-        return v
-      })
-    )
-    this.history = await chrome.history.search.promisify({ text: "", maxResults: 10, startTime: Date.now() - 90 * 24 * 60 * 60 * 1000 })
+    const history = await chrome.history.search.promisify({ text: "", maxResults: 0, startTime: Date.now() - 90 * 24 * 60 * 60 * 1000 })
+    const tabs = (await chrome.sessions.getDevices.promisify()).flatMap((v) => v.sessions).flatMap((v) => v.window.tabs)
+    this.history = Object.freeze(history)
+    this.tabs = Object.freeze(tabs)
+    this.bookmarks = Object.freeze(bookmarks)
     setInterval(() => (this.now = new Date().format("hh:mm")), 1000)
 
     const localHistory = await idb.get("history")
     const lastVisitTime = localHistory ? localHistory.map("lastVisitTime").max().ceil() : Date.now() - 90 * 24 * 60 * 60 * 1000
     const recentHistory = await chrome.history.search.promisify({ text: "", maxResults: 0, startTime: lastVisitTime })
-    const history = recentHistory.concat(localHistory || [])
-    this.history = Object.freeze(history)
-    await idb.set("history", history)
+    const fullHistory = recentHistory.concat(localHistory || [])
+    this.history = Object.freeze(fullHistory)
+    await idb.set("history", fullHistory)
   },
 })
 app.config.globalProperties.window = window
-app.mount("main")
+window.instance = app.mount("main")
